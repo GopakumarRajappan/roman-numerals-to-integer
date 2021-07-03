02ffa647f59bdaeb60efe96593a0362ca4018da8
GopakumarRajappan <gopakumar.rajappan01@gmail.com>
2021-07-03 20:05:42 +1000

    user input validation added.

--- RomanNumeralsToIntegerNew/src/ntt/roman/integer/conversion/RomanToIntegerConversion.java
+++ RomanNumeralsToIntegerNew/src/ntt/roman/integer/conversion/RomanToIntegerConversion.java
@@ -8,6 +8,7 @@ package ntt.roman.integer.conversion;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class RomanToIntegerConversion {
 
@@ -18,7 +19,7 @@ public class RomanToIntegerConversion {
 		 * HashMap used to store the key-value pairs since speed of operation 
 		 * is the constraint than order.
 		 **/
-		Map<String,Integer> romanIntegerMap = new HashMap<String, Integer>();
+		Map<String,Integer> romanIntegerMap = new TreeMap<String, Integer>();
 		romanIntegerMap.put("I", 1);
 		romanIntegerMap.put("V", 5);
 		romanIntegerMap.put("X", 10);
@@ -30,7 +31,7 @@ public class RomanToIntegerConversion {
 		/**
 		 * Accepting user input and closing scanner after use.
 		 */
-		System.out.print("Enter Roman numeral to be converted to integer: ");
+		System.out.println("Enter Roman numeral to be converted to integer: ");
 		Scanner scan = new Scanner(System.in);
 		String userRomanInput;
 		try{
@@ -38,9 +39,44 @@ public class RomanToIntegerConversion {
 		}finally {
 			scan.close();
 		}
-		System.out.print("User Roman Input : " + userRomanInput);
 		
+		userRomanInput = userRomanInput.toUpperCase();
+		/**
+		 * Validating user input for allowable range of Roman letters.
+		 * Range is from "I" to "MMMMM"
+		 */
+		try {
+			if (userRomanInput.length() < 1 || userRomanInput.length() > 5) {
+				throw new Exception();
+
+			}
 		
+		/**
+		 * Validating user input for invalid characters for Roman letters.
+		 */
+		boolean validValueCheck = false;		
+		for (int iterator=0; iterator < userRomanInput.length(); iterator++) {
+			System.out.print(userRomanInput.charAt(iterator));
+			for (Map.Entry<String,Integer> romanIntegerMapEntry : romanIntegerMap.entrySet()) {
+				if (userRomanInput.charAt(iterator) == romanIntegerMapEntry.getKey().charAt(0)) {
+					validValueCheck = true;
+					break;
+				}else {
+					validValueCheck = false;
+				}
+			}
+			if (validValueCheck == false) {
+				throw new Exception();	
+			}
+		}
+		
+		/**
+		 * Valid user input received.
+		 */
+		}catch (Exception exception) {
+			System.out.println("Please provide a valid Roman literal. " +
+		         "Valid Range is from \"I\" to \"MMMMM\".  Valid Roman literals are I, V, X, L , C, D , M");
+		}
 
 	}
 
